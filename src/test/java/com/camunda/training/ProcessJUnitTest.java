2050f1acc6d0775017b1b7184b5d42dc2e0179b8
JDFIREX <jonatandajtrowski@gmail.com>
2022-09-16 20:42:14 +0200

    exercise 3a - first test

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,30 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+
+@ExtendWith(ProcessEngineExtension.class)
 public class ProcessJUnitTest {
   
 //  @Test
+  @Test
+  @Deployment(resources = "twitter-qa-test.bpmn")
   public void testHappyPath() {
-    
+
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("approved", true);
+
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("twitter-qa",variables);
+
+    assertThat(processInstance).isEnded();
   }
 
 }
